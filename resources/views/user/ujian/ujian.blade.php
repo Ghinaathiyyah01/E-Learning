a2bb9bf953d79a79980105d7ca413a341193551b
seprinadwicahyani010 <seprinacahyani24@gmail.com>
2024-06-03 22:07:30 +0700

    ya sudah

--- resources/views/user/ujian/ujian.blade.php
+++ resources/views/user/ujian/ujian.blade.php
@@ -16,50 +16,36 @@
                                 <div class="form-group">
                                     <label style="font-size: 15px" for="jawaban_{{ $pertanyaan->id }}">{{ $questionNumber }}.
                                         {{ $pertanyaan->pertanyaan }}</label><br>
-                                    <input type="radio" name="jawaban[{{ $pertanyaan->id }}]"
+                                    <input type="radio" id="jawaban_{{ $pertanyaan->id }}_1" name="jawaban[{{ $pertanyaan->id }}]"
                                         value="{{ $pertanyaan->pilihan1 }}"> {{ $pertanyaan->pilihan1 }}<br>
-                                    <input type="radio" name="jawaban[{{ $pertanyaan->id }}]"
+                                    <input type="radio" id="jawaban_{{ $pertanyaan->id }}_2" name="jawaban[{{ $pertanyaan->id }}]"
                                         value="{{ $pertanyaan->pilihan2 }}"> {{ $pertanyaan->pilihan2 }}<br>
-                                    <input type="radio" name="jawaban[{{ $pertanyaan->id }}]"
+                                    <input type="radio" id="jawaban_{{ $pertanyaan->id }}_3" name="jawaban[{{ $pertanyaan->id }}]"
                                         value="{{ $pertanyaan->pilihan3 }}"> {{ $pertanyaan->pilihan3 }}<br>
-                                    <input type="radio" name="jawaban[{{ $pertanyaan->id }}]"
+                                    <input type="radio" id="jawaban_{{ $pertanyaan->id }}_4" name="jawaban[{{ $pertanyaan->id }}]"
                                         value="{{ $pertanyaan->pilihan4 }}"> {{ $pertanyaan->pilihan4 }}<br>
-                                    <small class="text-danger" id="validationMsg_{{ $pertanyaan->id }}"></small>
                                 </div>
                                 @php $questionNumber++; @endphp
                             @endforeach
-                            <button type="button" onclick="validateForm()" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
+                        <div id="notification" style="display: none;" class="alert alert-danger mt-3">
+                            Harap jawab semua pertanyaan sebelum mengirimkan formulir.
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <script>
-        function validateForm() {
-            let allQuestionsAnswered = true;
-            const form = document.getElementById('ujianForm');
-            const questions = form.querySelectorAll('input[type="radio"]');
-            
-            questions.forEach(question => {
-                const questionId = question.name.split('[')[1].split(']')[0];
-                const validationMsg = document.getElementById(`validationMsg_${questionId}`);
-                
-                if (!question.checked) {
-                    validationMsg.innerText = 'Harap pilih jawaban untuk pertanyaan ini.';
-                    allQuestionsAnswered = false;
-                } else {
-                    validationMsg.innerText = '';
-                }
-            });
-
-            if (allQuestionsAnswered) {
-                form.submit();
-            } else {
-                // Tidak melakukan submit karena ada pertanyaan yang belum dijawab
+        document.getElementById('ujianForm').addEventListener('submit', function(event) {
+            var radios = document.querySelectorAll('input[type=radio]:checked');
+            var totalQuestions = <?php echo count($soal); ?>;
+            if (radios.length !== totalQuestions) {
+                event.preventDefault(); // Prevent form submission
+                document.getElementById('notification').style.display = 'block'; // Show notification
             }
-        }
+        });
 
     </script>
 @endsection
